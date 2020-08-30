5a0a7e8a2cac533795f1dabd5dedfa1d551c6e4e
async-devil <async.devil@gmail.com>
2020-08-30 13:32:14 +0300

    Added more new custom types

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -5,6 +5,7 @@ import {GraphQLServer} from 'graphql-yoga'
 const typeDefs = `
   type Query {
     me: User!
+    greeting(name: String): String!
   }
 
   type Product {
@@ -14,19 +15,41 @@ const typeDefs = `
     rating: Float!
   }
 
+  type Post {
+    heading: String!
+    body: String!
+    published: Boolean!
+    id: ID!
+    author: User!
+    comments: Comment
+    rating: Float!
+  }
+
+  type Comment {
+    body: String!
+    published: Boolean!
+    id: ID!
+    author: User!
+    post: Post!
+  }
+
   type User {
     username: String!
     id: ID!
     email: String!
-    products: Product!
+    products: Product
+    posts: [Post]
+    comments: [Comment]
     age: Int
   }
 `
 
 //Resolvers
-
 const resolvers = {
   Query: {
+    greeting() {
+      return 'Hi'
+    },
     me() {
       return {
         username: 'test',
@@ -38,6 +61,40 @@ const resolvers = {
           stock: 2,
           rating: 5.3
         },
+        posts: [
+          {
+            heading: 'Test heading',
+            body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+            published: true,
+            id: '333-111-111',
+            //author:
+            //comments:
+            rating: 0
+          }, {
+            heading: 'Test heading 2',
+            body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+            published: true,
+            id: '333-111-112',
+            //author:
+            //comments:
+            rating: 0
+          }
+        ],
+        comments: [
+          {
+            body: 'Lorem ipsum dolor sit amet',
+            published: false,
+            id: '444-111-111'
+            //author
+            //post
+          }, {
+            body: 'Lorem ipsum dolor sit amet 2',
+            published: false,
+            id: '444-111-112'
+            //author
+            //post
+          }
+        ],
         age: 21
       }
     }
