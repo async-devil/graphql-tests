a8e53875e1061fedb7a7de8e3c791cb5fdca8271
async-devil <async.devil@gmail.com>
2020-09-05 17:13:06 +0300

    Fixed bug that I skipped

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -117,62 +117,64 @@ const resolvers = {
         } catch (e) {
           return []
         }
-        return doubleElementSearch(args.searchByAuthor, args.searchByTitle, posts, 'author', 'title')
-      },
-      comments(parent, args, ctx, info) {
-        if (args.searchByAuthor != '') {
-          var data = singleElementSearch(args.searchByAuthor, users, 'username')
-          try {
-            args.searchByAuthor = data[0].id
-          } catch (e) {
-            return []
-          }
+      }
+      return doubleElementSearch(args.searchByAuthor, args.searchByTitle, posts, 'author', 'title')
+    },
+    comments(parent, args, ctx, info) {
+      if (args.searchByAuthor != '') {
+        var data = singleElementSearch(args.searchByAuthor, users, 'username')
+        try {
+          args.searchByAuthor = data[0].id
+        } catch (e) {
+          return []
         }
-        return doubleElementSearch(args.searchByAuthor, args.searchByBody, comments, 'author', 'body')
-      },
-      me(parent, args, ctx, info) {
-        return {username: 'test', id: '111-111-111', email: 'test@test.com', age: 21}
       }
+      return doubleElementSearch(args.searchByAuthor, args.searchByBody, comments, 'author', 'body')
     },
-    Post: {
-      author(parent, args, ctx, info) {
-        return users.find((user) => {
-          return user.id === parent.author
-        })
-      },
-      comments(parent, args, ctx, info) {
-        return comments.filter(comment => {
-          return comment.post === parent.id
-        });
-      }
-    },
-    User: {
-      posts(parent, args, ctx, info) {
-        return posts.filter(post => {
-          return post.author === parent.id
-        });
-      },
-      comments(parent, args, ctx, info) {
-        return comments.filter(comment => {
-          return comment.author === parent.id
-        });
-      }
-    },
-    Comment: {
-      author(parent, args, ctx, info) {
-        return users.find((user) => {
-          return user.id === parent.author
-        })
-      },
-      post(parent, args, ctx, info) {
-        return users.find((post) => {
-          return post.id === parent.post
-        })
-      }
+    me(parent, args, ctx, info) {
+      return {username: 'test', id: '111-111-111', email: 'test@test.com', age: 21}
     }
-
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author
+      })
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter(comment => {
+        return comment.post === parent.id
+      });
+    }
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter(post => {
+        return post.author === parent.id
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter(comment => {
+        return comment.author === parent.id
+      });
+    }
+  },
+  Comment: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author
+      })
+    },
+    post(parent, args, ctx, info) {
+      return users.find((post) => {
+        return post.id === parent.post
+      })
+    }
   }
 
-  const server = new GraphQLServer({typeDefs, resolvers})server.start(opts, () => {
-    console.log(`server started on port ${opts.port}`);
-  })
+}
+
+const server = new GraphQLServer({typeDefs, resolvers})
+server.start(opts, () => {
+  console.log(`server started on port ${opts.port}`);
+})
