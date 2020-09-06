1fdffe85d6f771009f9e3de1a88617140829a47f
async-devil <async.devil@gmail.com>
2020-09-06 13:22:36 +0300

    Edited for new databese system and adding uses

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,30 +1,49 @@
 import {GraphQLServer} from 'graphql-yoga'
+// import uuidv4 from 'uuid/v4'
 const fs = require('fs');
 
 const singleElementSearch = require('./modules/singleElementSearch.js');
 const doubleElementSearch = require('./modules/doubleElementSearch.js');
 //const singleMultipleObjectSearch = require('./modules/singleMultipleObjectSearch.js');
+const addUser = require('./modules/addUser.js');
+
+// const pushAllData = function(users, posts, comments) {
+//   var data = [users, posts, comments,];
+//   add all data to one array
+//   fs.writeFileSync(`${__dirname}/database.json`, JSON.stringify(data));
+//   rewriting database.json file
+// }
+
+const pushData = (data, fileName) => {
+  fs.writeFileSync(`${__dirname}/database/${fileName}.json`, JSON.stringify(data));
+}
 
 const opts = {
+  //server options
   port: 4001
 }
-//demo db
-var data = JSON.parse(fs.readFileSync(`${__dirname}/database.json`))
 
-var {
-  users,
-  posts,
-  comments
-} = data
+try {
+  var users = JSON.parse(readFileSync(`${__dirname}/database/users.json`));
+  var posts = JSON.parse(readFileSync(`${__dirname}/database/posts.json`));
+  var comments = JSON.parse(readFileSync(`${__dirname}/database/comments.json`));
+} catch (e) {
+  var users = [];
+  var posts = [];
+  var comments = [];
+}
 
 //Type definitions
-
 const typeDefs = `
   type Query {
     me: User!
     posts(searchByAuthor: String, searchByTitle: String): [Post]
     comments(searchByAuthor: String, searchByBody: String): [Comment]
     users(searchByID: String, searchByUsername: String): [User]
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!): User!
   }
 
   type Post {
@@ -51,7 +70,6 @@ const typeDefs = `
     email: String!
     posts: [Post!]
     comments: [Comment!]
-    age: Int
   }
 `
 
@@ -85,7 +103,16 @@ const resolvers = {
       return doubleElementSearch(args.searchByAuthor, args.searchByBody, comments, 'author', 'body')
     },
     me(parent, args, ctx, info) {
-      return {username: 'test', id: '111-111-111', email: 'test@test.com', age: 21}
+      return {username: 'test', id: '111-111-111', email: 'test@test.com', age: 21,}
+    },
+  },
+  Mutation: {
+    createUser(parent, args, ctx, info) {
+      var newUser = addUser(users, args);
+      users.push(newUser)
+      pushData(users, 'users');
+
+      return newUser;
     }
   },
   Post: {
@@ -98,36 +125,37 @@ const resolvers = {
       return comments.filter(comment => {
         return comment.post === parent.id
       });
-    }
+    },
   },
   User: {
     posts(parent, args, ctx, info) {
       return posts.filter(post => {
         return post.author === parent.id
+        //if post author (id value) = user.id then user.posts = posts that filterd
       });
     },
     comments(parent, args, ctx, info) {
       return comments.filter(comment => {
         return comment.author === parent.id
       });
-    }
+    },
   },
   Comment: {
     author(parent, args, ctx, info) {
       return users.find((user) => {
         return user.id === parent.author
+        //if user.id = comment.author then comment.author = founded user
       })
     },
     post(parent, args, ctx, info) {
       return users.find((post) => {
         return post.id === parent.post
       })
-    }
-  }
-
+    },
+  },
 }
 
-const server = new GraphQLServer({typeDefs, resolvers})
+const server = new GraphQLServer({typeDefs, resolvers,})
 server.start(opts, () => {
   console.log(`server started on port ${opts.port}`);
 })
