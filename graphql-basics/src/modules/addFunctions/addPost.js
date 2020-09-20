7b648acd033b3c933b7823421af33df8ffd03780
async-devil <69465334+async-devil@users.noreply.github.com>
2020-09-20 17:34:45 +0300

    Refactored code

--- graphql-basics/src/modules/addFunctions/addPost.js
+++ graphql-basics/src/modules/addFunctions/addPost.js
@@ -1,42 +1,38 @@
-import uuidv4 from "uuid/v4";
+import uuidv4 from 'uuid/v4';
 import { validate as uuidValidate } from 'uuid';
 import validator from 'validator';
 
 function addPost(posts, args, users) {
-  try {
-    //checking if data is existing
-    var {
-      title,
-      body,
-      published,
-      author
-    } = args;
-    var data = Object.values(args) //creating array of args valuses
-  } catch (e) {
-    throw new Error("Invalid data");
-  };
+  const {
+    title, body, published, author,
+  } = args;
+  if (!title || !body || !published || !author) {
+    throw new Error('Invalid data');
+  }
 
-  data.forEach((item, i) => {
-    //checking if items not empty
-    if (typeof item == 'boolean') {
+  args.forEach((item) => {
+    // checking if items not empty
+    if (typeof item === 'boolean') {
       return;
     }
     if (validator.isEmpty(item)) {
-      throw new Error("Invalid data")
+      throw new Error('Invalid data');
     }
   });
 
-  if(!uuidValidate(author)) {
-    throw new Error("Invalid data")
+  if (!uuidValidate(author)) {
+    throw new Error('Invalid data');
   }
 
-  var userExists = users.some(user => user.id == author);
+  const userExists = users.some((user) => user.id === author);
   if (!userExists) {
-    throw new Error("User not found")
+    throw new Error('User not found');
   }
 
-  return {title, body, published, id: uuidv4(), author};
-  //returning newUser object
-};
+  return {
+    title, body, published, id: uuidv4(), author,
+  };
+  // returning newUser object
+}
 
 module.exports = addPost;
