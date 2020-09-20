208fd5a415cc369c2c046654b05c7b0296f9d3a0
async-devil <69465334+async-devil@users.noreply.github.com>
2020-09-20 18:01:20 +0300

    Refactored code

--- graphql-basics/src/modules/addFunctions/addComment.js
+++ graphql-basics/src/modules/addFunctions/addComment.js
@@ -1,50 +1,47 @@
-import uuidv4 from "uuid/v4";
+import uuidv4 from 'uuid/v4';
 import { validate as uuidValidate } from 'uuid';
 import validator from 'validator';
 
 function addComment(comments, args, users, posts) {
-  try {
-    //checking if data is existing
-    var {
-      body,
-      published,
-      author,
-      post
-    } = args;
-    var data = Object.values(args) //creating array of args valuses
-  } catch (e) {
-    throw new Error("Invalid data");
-  };
+  const {
+    body, published, author, post,
+  } = args;
 
-  data.forEach((item, i) => {
-    //checking if items not empty
-    if (typeof item == 'boolean') {
+  if (!body || !published || !author || !post) {
+    throw new Error('Invalid data');
+  }
+
+  args.forEach((item) => {
+    // checking if items not empty
+    if (typeof item === 'boolean') {
       return;
     }
     if (validator.isEmpty(item)) {
-      throw new Error("Invalid data")
+      throw new Error('Invalid data');
     }
   });
 
-  if(!uuidValidate(author)) {
-    throw new Error("Invalid data")
+  if (!uuidValidate(author)) {
+    throw new Error('Invalid data');
   }
-  if(!uuidValidate(post)) {
-    throw new Error("Invalid data")
+  if (!uuidValidate(post)) {
+    throw new Error('Invalid data');
   }
 
-  var userExists = users.some(user => user.id == author);
+  const userExists = users.some((user) => user.id === author);
   if (!userExists) {
-    throw new Error("User not found")
+    throw new Error('User not found');
   }
 
-  var postExists = posts.some(item => item.id == post);
+  const postExists = posts.some((item) => item.id === post);
   if (!postExists) {
-    throw new Error("Post not found")
+    throw new Error('Post not found');
   }
 
-  return {body, published, id: uuidv4(), author, post};
-  //returning newComment object
-};
+  return {
+    body, published, id: uuidv4(), author, post,
+  };
+  // returning newComment object
+}
 
 module.exports = addComment;
