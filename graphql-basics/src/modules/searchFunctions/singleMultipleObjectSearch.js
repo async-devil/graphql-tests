38a0d20823795438de4f0d8e62f0203207dd53cf
async-devil <69465334+async-devil@users.noreply.github.com>
2020-09-20 14:30:01 +0300

    Linted up and fixed some syntax issues in search modules

--- graphql-basics/src/modules/searchFunctions/singleMultipleObjectSearch.js
+++ graphql-basics/src/modules/searchFunctions/singleMultipleObjectSearch.js
@@ -1,13 +1,14 @@
 function singleMultipleObjectSearch(dataToSearch, dataBase, searchObject1, searchObject2) {
   try {
     return dataBase.filter((item) => {
-      return item[searchObject1][searchObject2].toLowerCase().includes(dataToSearch.toLowerCase());
+      const data = item[searchObject1][searchObject2];
+      return data.toLowerCase().includes(dataToSearch.toLowerCase());
     });
   } catch (err) {
     if (!dataToSearch && !dataBase && !searchObject1 && !searchObject2) {
-      return false
+      return false;
     }
-    return dataBase
+    return dataBase;
   }
 }
 
