6f90bb9090dde618e6fef3e1dd38ae2ae0adb6d6
async-devil <69465334+async-devil@users.noreply.github.com>
2020-09-20 22:13:02 +0300

    Refactored code

--- graphql-basics/src/modules/deleteFunctions/deletePost.js
+++ graphql-basics/src/modules/deleteFunctions/deletePost.js
@@ -1,18 +1,20 @@
 function removePost(posts, comments, args) {
-  var postIndex = posts.findIndex((post) => post.id === args.id)
-  
+  const postIndex = posts.findIndex((post) => post.id === args.id);
+  // finding post by id
+
   if (postIndex === -1) {
-    throw new Error('Post not found')
+    throw new Error('Post not found');
   }
 
-  var deletedPost = posts.splice(postIndex, 1);
+  const deletedPost = posts.splice(postIndex, 1);
 
-  comments = comments.filter((comment) => {
-    var match = comment.post === args.id;
-    return !match
-  })
+  const filteredComments = comments.filter((comment) => {
+    const match = comment.post === args.id;
+    return !match;
+    // returning comments which didin`t left on deleting post
+  });
 
-  return {posts, comments, deletedPost: deletedPost[0]}
+  return { posts, comments: filteredComments, deletedPost: deletedPost[0] };
 }
 
 module.exports = removePost;
