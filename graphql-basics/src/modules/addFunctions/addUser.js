066c31a2f6880abb944cd8c000a9a18f52bfa357
async-devil <69465334+async-devil@users.noreply.github.com>
2020-09-20 18:01:32 +0300

    Refactored code

--- graphql-basics/src/modules/addFunctions/addUser.js
+++ graphql-basics/src/modules/addFunctions/addUser.js
@@ -1,46 +1,42 @@
-import uuidv4 from "uuid/v4";
+import uuidv4 from 'uuid/v4';
 import validator from 'validator';
 
 function addUser(users, args) {
-  try {
-    //checking if data is existing
-    var {
-      username,
-      email
-    } = args;
-    var data = Object.values(args) //creating array of args valuses
-  } catch (e) {
-    throw new Error("Invalid data");
-  };
+  const { username, email } = args;
 
-  data.forEach((item, i) => {
-    //checking if items not empty
+  if (!username || !email) {
+    // checking if data is existing
+    throw new Error('Invalid data');
+  }
+
+  args.forEach((item) => {
+    // checking if items not empty
     if (validator.isEmpty(item)) {
-      throw new Error("Invalid data")
+      throw new Error('Invalid data');
     }
   });
 
   if (!validator.isEmail(email)) {
-    //checking if email valid
-    throw new Error("Invalid email")
+    // checking if email valid
+    throw new Error('Invalid email');
   }
-  
-  var emailTaken = users.some(user => user.email === email);
-  //checking if email used
-  var usernameTaken = users.some(user => users.username === username);
-  //checking if username used
+
+  const emailTaken = users.some((user) => user.email === email);
+  // checking if email used
+  const usernameTaken = users.some((user) => user.username === username);
+  // checking if username used
 
   if (emailTaken) {
-    //returning error if email has already taken
-    throw new Error("This email has taken");
-  };
+    // returning error if email has already taken
+    throw new Error('This email has taken');
+  }
   if (usernameTaken) {
-    //returning error if username has already taken
-    throw new Error("This email has taken");
-  };
+    // returning error if username has already taken
+    throw new Error('This username has taken');
+  }
 
-  return {username: args.username, id: uuidv4(), email: args.email,};
-  //returning newUser object
-};
+  return { username, id: uuidv4(), email };
+  // returning newUser object
+}
 
 module.exports = addUser;
