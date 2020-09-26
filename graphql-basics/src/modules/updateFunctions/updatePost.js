ad1fae883bd6768b86195bfdf9a516194a544e88
async-devil <async.devil@gmail.com>
2020-09-26 17:29:11 +0300

    Fixed bugs

--- graphql-basics/src/modules/updateFunctions/updatePost.js
+++ graphql-basics/src/modules/updateFunctions/updatePost.js
@@ -2,27 +2,27 @@ function updatePost(posts, args) {
   const {id, data} = args;
   let updatedPosts = posts;
 
-  const postToUpdate = posts.find((post) => post.id === data.id);
-  const postIndex = post.findIndex((post) => post.id === data.id);
+  const postToUpdate = posts.find((post) => post.id === id);
+  const postIndex = posts.findIndex((post) => post.id === id);
 
   if (!postToUpdate) {
     throw new Error("Post not find")
   }
 
   if (typeof data.title === 'string') {
-    userToUpdate.title = data.title;
+    postToUpdate.title = data.title;
   }
 
   if (typeof data.body === 'string') {
-    userToUpdate.body = data.body;
+    postToUpdate.body = data.body;
   }
 
   if (typeof data.published === 'boolean') {
-    userToUpdate.published = data.published;
+    postToUpdate.published = data.published;
   }
 
   updatedPosts[postIndex] = postToUpdate;
-  return {updatedPost: userToUpdate, updatedPosts};
+  return {updatedPost: postToUpdate, updatedPosts};
 }
 
 module.exports = updatePost;
