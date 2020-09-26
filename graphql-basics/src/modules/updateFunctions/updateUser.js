6e0480c198adf69634dde569ba84639d49fee138
async-devil <async.devil@gmail.com>
2020-09-26 14:12:20 +0300

    Finished updateUser method

--- graphql-basics/src/modules/updateFunctions/updateUser.js
+++ graphql-basics/src/modules/updateFunctions/updateUser.js
@@ -2,27 +2,31 @@ import validator from 'validator';
 
 function updateUser(users, args) {
   const { id, data } = args;
+  let updatedUsers = users;
 
   const userToUpdate = users.find((user) => user.id === id);
+  const userIndex = users.findIndex((user) => user.id === args.id);
+
   if (!userToUpdate) {
     throw new Error('User not found');
   }
 
   if (typeof data.email === 'string' && validator.isEmail(data.email)) {
     const emailTaken = users.some((user) => user.email === data.email);
-
-    if (emailTaken) {
+    console.log(data.email, userToUpdate.email);
+    if (emailTaken && data.email !== userToUpdate.email) {
       throw new Error('Email has taken');
     }
 
     userToUpdate.email = data.email;
-  }
+  } else throw new Error('Invalid data');
 
   if (typeof data.username === 'string' && !validator.isEmpty(data.username)) {
     userToUpdate.username = data.username;
-  }
+  } else throw new Error('Invalid data');
 
-  return userToUpdate;
+  updatedUsers[userIndex] = userToUpdate
+  return {updatedUser: userToUpdate, updatedUsers};
 }
 
 module.exports = updateUser;
