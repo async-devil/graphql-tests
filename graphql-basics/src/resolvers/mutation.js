1a1101fa4ca27ac0a3c80ef82eb9a03785030b3d
async-devil <async.devil@gmail.com>
2020-09-26 17:28:51 +0300

    Edited mutation for updatePost method

--- graphql-basics/src/resolvers/mutation.js
+++ graphql-basics/src/resolvers/mutation.js
@@ -1,12 +1,17 @@
 const addUser = require('../modules/addFunctions/addUser.js');
 const addPost = require('../modules/addFunctions/addPost.js');
 const addComments = require('../modules/addFunctions/addComment.js');
+
 const removeUser = require('../modules/deleteFunctions/deleteUser.js');
 const removePost = require('../modules/deleteFunctions/deletePost.js');
 const removeComment = require('../modules/deleteFunctions/deleteComment.js');
+
 const updateUser = require('../modules/updateFunctions/updateUser.js');
+const updatePost = require('../modules/updateFunctions/updatePost.js');
 
 const mutation = {
+
+  /*Users mutations*/
   createUser(parent, args, {
     db,
   }) {
@@ -14,6 +19,7 @@ const mutation = {
     db.users.push(user);
     return user;
   },
+
   deleteUser(parent, args, {
     db,
   }) {
@@ -23,6 +29,7 @@ const mutation = {
     db.comments = data.comments;
     return data.deletedUser;
   },
+
   updateUser(parent, args, {
     db,
   }) {
@@ -30,6 +37,9 @@ const mutation = {
     db.users = data.updatedUsers;
     return data.updatedUser
   },
+  /* */
+
+  /*Posts mutations */
   createPost(parent, args, {
     db,
   }) {
@@ -37,6 +47,7 @@ const mutation = {
     db.posts.push(post);
     return post;
   },
+
   deletePost(parent, args, {
     db,
   }) {
@@ -45,6 +56,17 @@ const mutation = {
     db.comments = data.comments;
     return data.deletedPost;
   },
+
+  updatePost(parent, args, {
+    db,
+  }) {
+    const data = updatePost(db.posts, args);
+    db.posts = data.updatedPosts;
+    return data.updatedPost
+  },
+  /* */
+
+  /*Comments mutations */
   createComment(parent, args, {
     db,
   }) {
@@ -52,6 +74,7 @@ const mutation = {
     db.comments.push(comment);
     return comment;
   },
+
   deleteComment(parent, args, {
     db,
   }) {
@@ -59,7 +82,9 @@ const mutation = {
     db.comments = data.comments;
     return data.deletedComment;
   },
+  /* */
 };
+
 export {
   mutation as
   default,
