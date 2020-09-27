6975c13a296f7fade1c53c8ef09b810e93006bda
async-devil <async.devil@gmail.com>
2020-09-27 13:46:56 +0300

    Added support for subscriptions

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,9 +1,12 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 
 /* Custom modules requirement */
 import db from './db';
+
 import Query from './resolvers/query';
 import Mutation from './resolvers/mutation';
+import Subscription from './resolvers/subscription'
+
 import User from './resolvers/user';
 import Post from './resolvers/post';
 import Comment from './resolvers/comment';
@@ -11,9 +14,11 @@ import Comment from './resolvers/comment';
 
 /* Server options */
 const opts = {
-  port: 4003,
+  port: 4000,
 };
 /*  */
+
+const pubsub = new PubSub();
 
 /* Configurating test server */
 const server = new GraphQLServer({
@@ -21,12 +26,14 @@ const server = new GraphQLServer({
   resolvers: {
     Query,
     Mutation,
+    Subscription,
     User,
     Post,
     Comment,
   },
   context: {
     db,
+    pubsub,
   },
 });
 /*  */
