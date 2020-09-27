9d7e67c2a9e83b2d1f4b0a0715bc8e2624a66821
async-devil <async.devil@gmail.com>
2020-09-27 13:45:26 +0300

    Added some comments

--- graphql-basics/src/resolvers/post.js
+++ graphql-basics/src/resolvers/post.js
@@ -3,11 +3,13 @@ const post = {
     db,
   }) {
     return db.users.find((user) => user.id === parent.author);
+    //searching for author by post.id
   },
   comments(parent, args, {
     db,
   }) {
     return db.comments.filter((comment) => comment.post === parent.id);
+    //searching for comments by post.id
   },
 };
 
