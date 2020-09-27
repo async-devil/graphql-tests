0e88bf9b8482d39175f9a82383f70d9ae10a762f
async-devil <async.devil@gmail.com>
2020-09-27 13:45:50 +0300

    Added some comments

--- graphql-basics/src/resolvers/user.js
+++ graphql-basics/src/resolvers/user.js
@@ -3,12 +3,13 @@ const user = {
     db,
   }) {
     return db.posts.filter((post) => post.author === parent.id);
-    // if post author (id value) = user.id then user.posts = posts that filterd
+    // if post.author(id value) equals to user.id then user posts reassignment to posts that filterd
   },
   comments(parent, args, {
     db,
   }) {
     return db.comments.filter((comment) => comment.author === parent.id);
+    //the same thing but with comments
   },
 };
 
