11e434696903e1e7a112e599ed480e940b0e899f
async-devil <async.devil@gmail.com>
2020-09-27 13:45:40 +0300

    Added some comments

--- graphql-basics/src/resolvers/comment.js
+++ graphql-basics/src/resolvers/comment.js
@@ -3,11 +3,13 @@ const comment = {
     db,
   }) {
     return db.users.find((user) => user.id === parent.author);
+    //returning user which id equals to comment.author(user.id)
   },
   post(parent, args, {
     db,
   }) {
     return db.posts.find((post) => post.id === parent.post);
+    //the same thing but with post
   },
 };
 
