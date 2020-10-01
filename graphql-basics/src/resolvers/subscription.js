3652583fc94a5e39a31c932894b637a933b9d1b1
async-devil <69465334+async-devil@users.noreply.github.com>
2020-10-01 22:29:02 +0300

    Fixed bug

--- graphql-basics/src/resolvers/subscription.js
+++ graphql-basics/src/resolvers/subscription.js
@@ -1,18 +1,18 @@
 const subscription = {
   comment: {
     subscribe(parent, { postID }, { db, pubsub }, info) {
-      const post = db.posts.find((post) => post.id === postID && post.published);
+      const subscriptionPost = db.posts.find((post) => post.id === postID && post.published);
 
-      if (!post) throw new Error('Post not found');
+      if (!subscriptionPost) throw new Error('Post not found');
 
       return pubsub.asyncIterator(`comment on ${postID}`);
     },
   },
   post: {
     subscribe(parent, { userID }, { db, pubsub }, info) {
-      const user = db.users.find((user) => user.id === userID);
+      const subscriptionUser = db.users.find((user) => user.id === userID);
 
-      if (!user) throw new Error('User not find');
+      if (!subscriptionUser) throw new Error('User not find');
 
       return pubsub.asyncIterator(`post by ${userID}`);
     },
